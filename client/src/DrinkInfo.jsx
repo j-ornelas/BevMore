0b2494496ea12717877198df898f2ead94f980b1
j-ornelas <johnnyoh1988@gmail.com>
2018-03-31 17:11:48 -0700

    Adds state to drink listing

--- client/src/DrinkInfo.jsx
+++ client/src/DrinkInfo.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import axios from 'axios';
+import helpers from './helpers.js'
 
-export default function DrinkInfo (props){
-  let drinkURL = props.info.strDrinkThumb
-  return (
-    <div className="drink">
-      <span><img className="drinkpicture" src={props.info.strDrinkThumb} /></span>
-      <span className="drinkname">{props.info.strDrink}</span>
-      <span className="drinkingredients">drink ingredients</span>
-    </div>
-  )
+export default class DrinkInfo extends React.Component{
+  // let drinkURL = props.info.strDrinkThumb
+  constructor(props){
+    super(props);
+    this.state = {
+      info: props.info,
+      drinkIngredients: [],
+      instructions: ""
+    }
+  }
+
+  findIngredients(){
+    let query = this.state.info.idDrink;
+    console.log(query)
+    let context = this;
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${query}`)
+      .then(function (response) {
+        context.setState({drinkIngredients:helpers.formatIngredients(response.data.drinks[0])})
+        context.setState({instructions:response.data.drinks[0].strInstructions})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  render(){
+    let ingredientsList = this.state.drinkIngredients.map((ingredient) =>
+      <li>{ingredient[0] + " - " + ingredient[1]}</li>
+    )
+
+    return (
+      <div className="drink">
+      <span><img className="drinkpicture" src={this.state.info.strDrinkThumb} /></span>
+      <span className="drinkname">{this.state.info.strDrink} <button onClick={this.findIngredients.bind(this)}>more info</button></span>
+      <ul className="drinkingredients">{ingredientsList}</ul>
+      <span>{this.state.instructions}</span>
+      </div>
+    )
+  }
 }
