b28af4c7785296345b78ec0cf913ebc097da57e0
j-ornelas <johnnyoh1988@gmail.com>
2018-03-31 17:12:25 -0700

    Removes hardcoded ingredients, adds input field

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import axios from "axios";
 import helpers from "./helpers.js";
 import NavBar from "./Navbar.jsx";
+import Ingredients from "./Ingredients.jsx";
 import DrinkList from "./DrinkList.jsx";
 
 class Main extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      ingredients: ["Gin", "Orange_Juice", "Grenadine"],
+      query: "",
+      ingToAdd: "",
+      ingredients: [],
       drinks: []
     }
 
@@ -20,8 +23,8 @@ class Main extends React.Component {
 
   getInfo(ingredientsArray){
     let query = helpers.formatQuery(this.state.ingredients);
+    console.log(query)
     let context = this;
-
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/${query}`)
       .then(function (response) {
         context.setState({drinks:response.data.drinks})
@@ -29,16 +32,37 @@ class Main extends React.Component {
     .catch(function (error) {
       console.log(error);
     });
+  }
 
+  addIng(str){
 
   }
+
+  addIng(){
+    console.log(this.state.ingToAdd)
+    let currentIng = this.state.ingredients
+    currentIng.push(helpers.formatIngredient(this.state.ingToAdd))
+    this.setState({ingredients:currentIng});
+    this.setState({ingToAdd:""})
+    this.getInfo(this.state.ingredients)
+  }
+
+  handleChange(event){
+    this.setState({ingToAdd: event.target.value})
+  }
+
 
   render(){
     return (
       <div>
-      <NavBar />
-      <DrinkList drinks={this.state.drinks}/>
-      <button onClick={this.getInfo.bind(this, this.state.ingredients)}>test</button>
+        <NavBar />
+        <Ingredients
+          ingredients={this.state.ingredients}
+          ingToAdd={this.state.ingToAdd}
+          addIng={this.addIng.bind(this)}
+          handleChange={this.handleChange.bind(this)}
+        />
+        <DrinkList drinks={this.state.drinks}/>
       </div>
     )
   }
